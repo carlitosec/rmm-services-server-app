4d2abd0a4622515805794433c9d2587719723ffe
Carlos Carrillo <carlossmonk@hotmail.com>
2021-05-18 00:36:35 -0500

    Secure API implementation

---
+++ src/main/java/com/rmm/config/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.rmm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author ccarrillo
+ *
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	/* (non-Javadoc)
+	 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	}
+}
